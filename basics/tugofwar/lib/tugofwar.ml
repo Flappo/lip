424a9ff442cad410fbc569fba0f4a110db6d28e8
flappo25 <flappopk@gmail.com>
2023-10-16 17:53:19 +0200

    tugofwar quasi completato.

--- basics/tugofwar/lib/tugofwar.ml
+++ basics/tugofwar/lib/tugofwar.ml
@@ -10,25 +10,58 @@ let explode s =
     if i < 0 then l else exp (i - 1) (s.[i] :: l) in
   exp (String.length s - 1) []
 
-let rec toklist_of_string s = 
-  let s_list = explode s 
-match s_list with
-| 'A'::tl -> A::toklist_of_string tl
-| '='::tl -> X::toklist_of_string tl
-| 'B'::tl -> B::toklist_of_string tl
-
-
-failwith "TODO"
+let toklist_of_string s =
+  if s = ""
+    then failwith "error"
+else (
+  let explosion = explode s
+in toklist_of_string s
+  let rec toklist_of_string_explode explosion = match explosion with
+  | 'A'::tl -> A::toklist_of_string_explode tl
+  | 'B'::tl -> B::toklist_of_string_explode tl
+  | '='::tl -> X::toklist_of_string_explode tl
+  | [] -> []
+  | _::_ -> failwith "error"
+in toklist_of_string explosion
+)
 
 (* val valid : token list -> bool *)
 (* valid l is true when l is a list of tokens in the language A* X* B* *)
     
-let valid l = failwith "TODO"
+let valid l = match l with
+| B::_ | [] -> false
+| A::B::_ -> false
+| A::tl | X::tl -> let rec validA tl = match tl with
+           | B::_ | [] -> false
+           | A::tl -> validA tl
+           | X::tl -> let rec validX tl = match tl with
+                      | B::[] | [] -> true
+                      | A::_ -> false
+                      | X::tl -> validX tl
+                      | B::tl -> validX tl
+           in validX tl
+in validA tl
+
 
 (* val win : token list -> token *)
 (* win l determines the winner of a tug of war game. X means tie *)
 
-let win l = failwith "TODO"
+let rec counterA l = match l with
+| [] | X::_ | B::_ -> 0
+| A::tl -> 1 + counterA tl
+
+let rec counterB l = match l with
+| A::tl | X::tl -> 0 + counterB tl
+| B::tl -> 1 + counterB tl
+| [] -> 0
+let win l = if (counterA l > counterB l) 
+              then A
+            else if (counterA l < counterB l) 
+              then B
+            else X
 
 (* val string_of_winner : token -> string *)
-let string_of_winner w = failwith "TODO"
+let string_of_winner w = match w with
+| A -> "the winner is A!"
+| B -> "the winner is B!"
+| X -> "there's a tie!" 
